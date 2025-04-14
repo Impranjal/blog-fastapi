1c0b003bc76b3299bcaebd27f8d75d49e5062c33
pranjal singh <singhpranjal2312@gmail.com>
2025-04-15 00:18:24 +0530

    Database connection

---
+++ blog/database.py
@@ -0,0 +1,26 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+class Blog(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    content: str | None = Field(default=None, index=True)
+    author_name: str
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 from schema import Blogs
+from database import *
+import uvicorn
+
 app = FastAPI()
+@app.on_event("startup")
+def load_all_data():
+    create_db_and_tables()
+
+@app.post('/data')
+def add_data(request:Blog,session:SessionDep):
+    session.add(request)
+    session.commit()
+    session.refresh(request)
+    return request
+
+@app.get('/blogs_data')
+def fetching_all_data(session:SessionDep):
+    blogs = session.exec(select(Blog)).all()
+    return blogs
 
 @app.post('/blog')
 def create(blogs:Blogs):
-    return f"My name is pranjal singh this is my first blog title {blogs.title} having contents as {blogs.contents} and is getting published on {blogs.published_date}"
+    return blogs
+if __name__ == "__main__":
+    uvicorn.run('main:app',host='127.0.0.1',port=5051,reload=True)
