2c32cb22add826333f2edf706e25439a71c24266
pranjal singh <singhpranjal2312@gmail.com>
2025-04-15 22:10:27 +0530

    Addition of deletion of records

--- blog/main.py
+++ blog/main.py
@@ -16,12 +16,22 @@ def add_data(request:Blog,session:SessionDep):
     return request
 
 @app.get('/blogs_data/{id}')
-def fetching_all_data(id,session:SessionDep):
-    blogs = session.exec(select(Blog)).filter(Blog.id==id)
-    return blogs
+def fetching_all_data(id:int,session:SessionDep):
+    blogs = session.exec(select(Blog).where(Blog.id==id))
+    return blogs.first()
 
 @app.post('/blog')
 def create(blogs:Blogs):
     return blogs
+
+@app.delete('/blog_del/{id}',status_code=204 )
+def destroy(id:int,session:SessionDep):
+    raw_id = session.get(Blog,id)
+    if not raw_id:
+        raise HTTPException(status_code=404,detail="Blog record not found")
+    session.delete(raw_id)
+    session.commit()
+    return {'ok':True}
+
 if __name__ == "__main__":
     uvicorn.run('main:app',host='127.0.0.1',port=5051,reload=True)
